0e94febb36a3ac67dea44fd71ff35bd83a5ddbad
Hung Anh <anhlhpl52119@gmail.com>
2025-05-25 20:31:53 +0700

    chore: update deps

---
+++ app.js
@@ -0,0 +1,8 @@
+const response = await fetch('https://example.com');
+
+const body = await response.text();
+console.log("this is body and controller and my story",body);
+
+function CreateNewTemplateForm () { 
+ // write down your function to handle this issue when the controller is come and
+}
